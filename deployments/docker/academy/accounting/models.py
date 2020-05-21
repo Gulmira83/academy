3bcc235a229e77b9a727e34fab47375900e176f8
JbFiras <contact@firas-jebari.tn>
2020-05-21 05:00:05 +0200

    subscription added to footer

--- deployments/docker/academy/accounting/models.py
+++ deployments/docker/academy/accounting/models.py
@@ -10,3 +10,36 @@ from django.db import models
 #     class Meta:
 #         managed     = True
 #         ordering    = ['name']
+
+
+class Plans(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.CharField(max_length=20)
+    description = models.CharField(max_length=250)
+    option1 = models.CharField(max_length=200)
+    option2 = models.CharField(max_length=200)
+    option3 = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "plans"
+
+
+# class Features(models.Model):
+#     option1 = models.CharField(max_length=200)
+#     option2 = models.CharField(max_length=200)
+#     option3 = models.CharField(max_length=200)
+
+#     plan = models.ForeignKey(Plans, on_delete=models.CASCADE) 
+
+#     def __str__(self):
+#         return self.option1
+       
+
+#     class Meta:
+#         verbose_name_plural = "features"
+
+
+    
