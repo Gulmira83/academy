0d84f9f6f3fe381d2e438f4b4140e904ac153714
JbFiras <contact@firas-jebari.tn>
2020-05-28 16:01:09 +0200

    paypal payment fixed in front-end and backend

--- deployments/docker/academy/accounting/views.py
+++ deployments/docker/academy/accounting/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Plans
 
+from main.models import Feature
+from django.http import JsonResponse
+import json
+
+
 # Create your views here.
 
 #we are passing the id of the plan with the url
@@ -9,3 +14,12 @@ def index(request, id):
         plan = Plans.objects.filter(id=id)
 
     return render(request, 'paypal.html', {'plan':plan})
+
+
+
+def paymentComplete(request):
+    body = json.loads(request.body)
+    plan = Plans.objects.get(id=body['productId'])
+    feature = Feature.objects.filter(user=request.user).update(payment_confirmation='True',feature_type=str(plan))
+
+    return JsonResponse('payment completed!', safe=False)
